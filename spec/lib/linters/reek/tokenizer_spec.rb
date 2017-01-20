6c79e30b0cd9b534c48677e69c6918673abe22ce
Joel Oliveira <joel.oliveira@gmail.com>
2017-01-20 16:16:51 -0500

    Better 4 phase setup

--- spec/lib/linters/reek/tokenizer_spec.rb
+++ spec/lib/linters/reek/tokenizer_spec.rb
@@ -4,14 +4,11 @@ require "linters/reek/tokenizer"
 describe Linters::Reek::Tokenizer do
   describe "#parse" do
     it "parses line numbers when columns provided" do
+      input = "test.rb:9: ModuleName: LintersHelper has no descriptive comment"
+      expected_message = "ModuleName: LintersHelper has no descriptive comment"
       tokenizer = Linters::Reek::Tokenizer.new
 
-      parsed = tokenizer.parse <<~OUTPUT
-        test.rb:9: IrresponsibleModule: LintersHelper has no descriptive comment
-      OUTPUT
-
-      expected_message =
-        "IrresponsibleModule: LintersHelper has no descriptive comment"
+      parsed = tokenizer.parse(input)
 
       expect(parsed).to eq([{ line: 9, message: expected_message }])
     end
