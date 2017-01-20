0b4589313600e391f479f2bbab7533ec0d29f210
Joel Oliveira <joel.oliveira@gmail.com>
2017-01-20 16:24:26 -0500

    DRY up error messages

--- lib/linters/base/options.rb
+++ lib/linters/base/options.rb
@@ -7,29 +7,20 @@ module Linters
       # @param _filename [String] relative path to the file being checked
       # @return [String] cli command to run
       def command(_filename)
-        raise(
-          NotImplementedError,
-          "implement #command in your Options class"
-        )
+        raise NotImplementedError, error_message_for(:command)
       end
 
       # Filename that the linter will look for the config by default.
       # @return [String] path and filename of the config file the linter wants
       def config_filename
-        raise(
-          NotImplementedError,
-          "implement #config_filename in your Options class"
-        )
+        raise NotImplementedError, error_message_for(:config_filename)
       end
 
       # Object to parse linter output, returning hash with line numbers and
       # messages of the issues
       # @return [#parse] a Tokenizer object implementing `#parse`
       def tokenizer
-        raise(
-          NotImplementedError,
-          "implement #tokenizer in your Options class"
-        )
+        raise NotImplementedError, error_message_for(:tokenizer)
       end
 
       # Returns content of linter's configuration file.  If the configuration
@@ -38,10 +29,13 @@ module Linters
       # @param _content [String] contents of a config file
       # @return [Sring, nil] the contents of the config file or nil
       def config_content(_content)
-        raise(
-          NotImplementedError,
-          "implement #config_content in your Options class"
-        )
+        raise NotImplementedError, error_message_for(:config_content)
+      end
+
+      private
+
+      def error_message_for(method)
+        "implement ##{method} in your Options class"
       end
     end
   end
